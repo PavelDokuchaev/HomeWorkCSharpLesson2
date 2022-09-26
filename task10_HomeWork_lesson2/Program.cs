8eab40deb575a060109dc28ca61ad72c03d731a3
dokuchaev.pa <dokuchaev.pa@gmail.com>
2022-09-26 18:33:42 +0400

    task10

---
+++ task10_HomeWork_lesson2/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+int getSecondDigitFromNumber(int number)
+{
+    int secondNumber = (number / 10) % 10;
+    return secondNumber;
+}
+
+Console.WriteLine("Введите трехзначное число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = getSecondDigitFromNumber(userNumber);
+Console.WriteLine($"Для числа {userNumber} вторая цифра {secondNumber}");
+
